ce736063264ad0335de4c243972fb54853eb7dcd
Spandan Dutta <spdutta@microsoft.com>
2025-01-31 19:17:33 -0800

    push

--- services/a2/berolea2/IBackgroundTaskQueue.cs
+++ services/a2/berolea2/IBackgroundTaskQueue.cs
@@ -1,6 +1,13 @@
-﻿namespace berolea2
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace berolea2
 {
     internal interface IBackgroundTaskQueue
     {
+        void QueueBackgroundWorkItem(Func<IServiceProvider, CancellationToken, Task> workItem);
+
+        Task<Func<IServiceProvider, CancellationToken, Task>> DequeueAsync(CancellationToken cancellationToken);
     }
 }
